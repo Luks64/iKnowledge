8a886b4dfaa9b6a73b2a2d9f62d666d356529107
Baluku Arnold <balukslynn@gmail.com>
2020-06-30 22:33:15 +0300

    Add project files.

---
+++ iKnowledge/Models/Code.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace iKnowledge.Models
+{
+    public class Code
+    {
+        public long Id { get; set; }
+        public long ProgrammingLanguageId { get; set; }
+        public string Explanation { get; set; }
+        public string Snippet { get; set; }
+        public long CreatedById { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
